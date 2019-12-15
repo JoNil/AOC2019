01a94ca71498cc3830c83890c9e7187c348e5269
Jonathan Nilsson <jonathan@voysys.se>
2019-12-15 21:55:31 +0100

    Day 15 A

--- day_15_a/src/main.rs
+++ day_15_a/src/main.rs
@@ -1,13 +1,14 @@
 use crossterm::{
     cursor,
-    style::{style, Color, PrintStyledContent, Print},
     event::{read, Event, KeyCode, KeyEvent},
+    style::{style, Color, Print, PrintStyledContent},
     terminal, ExecutableCommand,
 };
 use int_comp::{IntcodeComputer, IntcodeOutput};
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
-use std::io::stdout;
 use std::fs;
+use std::io::stdout;
 
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 enum Input {
@@ -18,7 +19,6 @@ enum Input {
 }
 
 impl Input {
-
     fn as_i32(self) -> i32 {
         match self {
             Input::North => 1,
@@ -47,8 +47,102 @@ impl Output {
     }
 }
 
-fn update(pos: &mut (i32, i32), input: Input, output: Output) -> Vec<((i32, i32), char)> {
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+enum Tile {
+    Wall,
+    Ground,
+}
 
+fn reconstruct_path(
+    came_from: &HashMap<(i32, i32), (i32, i32)>,
+    mut current: (i32, i32),
+) -> Vec<(i32, i32)> {
+    let mut total_path = Vec::new();
+    total_path.push(current);
+    while came_from.contains_key(&current) {
+        current = came_from[&current];
+        total_path.push(current);
+    }
+    total_path.reverse();
+    total_path
+}
+
+fn huristic(pos: (i32, i32), goal: (i32, i32)) -> i32 {
+    (goal.0 - pos.0).abs() + (goal.1 - pos.1).abs()
+}
+
+fn neighbors(pos: (i32, i32), map: &HashMap<(i32, i32), Tile>) -> Vec<(i32, i32)> {
+    let mut res = Vec::new();
+    for candidate in &[
+        (pos.0 + 1, pos.1),
+        (pos.0 - 1, pos.1),
+        (pos.0, pos.1 + 1),
+        (pos.0, pos.1 - 1),
+    ] {
+        if *map.get(candidate).unwrap_or(&Tile::Wall) == Tile::Ground {
+            res.push(*candidate);
+        }
+    }
+
+    res
+}
+
+fn a_star(
+    start: (i32, i32),
+    goal: (i32, i32),
+    map: &HashMap<(i32, i32), Tile>,
+) -> Option<Vec<(i32, i32)>> {
+    let mut open_set = HashSet::new();
+    open_set.insert(start);
+
+    let mut came_from = HashMap::new();
+
+    let mut g_score = HashMap::new();
+    g_score.insert(start, 0);
+
+    let mut f_score = HashMap::new();
+    f_score.insert(start, huristic(start, goal));
+
+    while !open_set.is_empty() {
+        let current = *open_set
+            .iter()
+            .min_by(|x, y| {
+                f_score
+                    .get(x)
+                    .unwrap_or(&std::i32::MAX)
+                    .cmp(f_score.get(y).unwrap_or(&std::i32::MAX))
+            })
+            .unwrap();
+
+        if current == goal {
+            return Some(reconstruct_path(&came_from, current));
+        }
+
+        open_set.remove(&current);
+
+        for neighbor in &neighbors(current, map) {
+            let tentative_g_score = g_score.get(&current).unwrap_or(&std::i32::MAX) + 1;
+
+            if tentative_g_score < *g_score.get(neighbor).unwrap_or(&std::i32::MAX) {
+                came_from.insert(*neighbor, current);
+                g_score.insert(*neighbor, tentative_g_score);
+                f_score.insert(*neighbor, tentative_g_score + huristic(*neighbor, goal));
+
+                open_set.insert(*neighbor);
+            }
+        }
+    }
+
+    None
+}
+
+fn update(
+    pos: &mut (i32, i32),
+    map: &mut HashMap<(i32, i32), Tile>,
+    last_path_to_home: &mut Vec<(i32, i32)>,
+    input: Input,
+    output: Output,
+) -> Option<Vec<((i32, i32), char, Color)>> {
     let mut res = Vec::new();
 
     let old_pos = *pos;
@@ -61,18 +155,37 @@ fn update(pos: &mut (i32, i32), input: Input, output: Output) -> Vec<((i32, i32)
     };
 
     if output == Output::MovedDone {
-        panic!("Done: {:?}", new_pos);
+        if let Some(path_to_home) = a_star((0, 0), new_pos, map) {
+            *last_path_to_home = path_to_home;
+        }
+        *pos = new_pos;
+        return None;
     }
 
     if output == Output::HitWall {
-        res.push((new_pos, '#'));
+        res.push((new_pos, '#', Color::Grey));
+        map.insert(new_pos, Tile::Wall);
     } else {
-        res.push((old_pos, '.'));
-        res.push((new_pos, 'D'));
+        res.push((old_pos, '.', Color::Grey));
+        res.push((new_pos, 'D', Color::Grey));
+        map.insert(new_pos, Tile::Ground);
         *pos = new_pos;
     }
 
-    res
+    if let Some(path_to_home) = a_star((0, 0), new_pos, map) {
+
+        for pos in last_path_to_home.iter() {
+            res.push((*pos, '.', Color::Grey));
+        }
+
+        for pos in path_to_home.iter() {
+            res.push((*pos, '.', Color::Blue));
+        }
+
+        *last_path_to_home = path_to_home;
+    }
+
+    Some(res)
 }
 
 const DRAW_OFFSET: (i32, i32) = (50, 50);
@@ -80,46 +193,65 @@ const DRAW_OFFSET: (i32, i32) = (50, 50);
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input")?;
 
-    let mut program = input
+    let program = input
         .split(",")
         .map(|v| v.trim().parse::<i64>())
         .collect::<Result<Vec<_>, _>>()?;
 
     let mut int_comp = IntcodeComputer::new(&program);
-
+    let mut map = HashMap::new();
     let mut pos = (0, 0);
+    let mut last_path_to_home = Vec::new();
+
+    map.insert(pos, Tile::Ground);
 
     stdout()
         .execute(terminal::Clear(terminal::ClearType::All))?
-        .execute(cursor::MoveTo((pos.0 + DRAW_OFFSET.0) as u16, (pos.1 + DRAW_OFFSET.1) as u16))?
+        .execute(cursor::MoveTo(
+            (pos.0 + DRAW_OFFSET.0) as u16,
+            (pos.1 + DRAW_OFFSET.1) as u16,
+        ))?
         .execute(Print('D'))?;
 
     loop {
-
         let input = match read()? {
-            Event::Key(KeyEvent { code: KeyCode::Up, .. }) => Input::South,
-            Event::Key(KeyEvent { code: KeyCode::Down, .. }) => Input::North,
-            Event::Key(KeyEvent { code: KeyCode::Left, .. }) => Input::West,
-            Event::Key(KeyEvent { code: KeyCode::Right, .. }) => Input::East,
+            Event::Key(KeyEvent {
+                code: KeyCode::Up, ..
+            }) => Input::South,
+            Event::Key(KeyEvent {
+                code: KeyCode::Down,
+                ..
+            }) => Input::North,
+            Event::Key(KeyEvent {
+                code: KeyCode::Left,
+                ..
+            }) => Input::West,
+            Event::Key(KeyEvent {
+                code: KeyCode::Right,
+                ..
+            }) => Input::East,
             _ => continue,
         };
 
         let output = match int_comp.run(&[input.as_i32() as i64], Some(1))? {
-            IntcodeOutput::Interrupt(output) => {
-                Output::from_i32(output[0] as i32)
-            }
+            IntcodeOutput::Interrupt(output) => Output::from_i32(output[0] as i32),
             IntcodeOutput::Halt(_) => {
-                panic!();
+                return Err("Halt".into());
             }
         };
 
-        let draw_instructions = update(&mut pos, input, output);
-
-        for (pos, ch) in draw_instructions {
-
-            stdout()
-                .execute(cursor::MoveTo((pos.0 + DRAW_OFFSET.0) as u16, (pos.1 + DRAW_OFFSET.1) as u16))?
-                .execute(Print(ch))?;
+        if let Some(draw_instructions) = update(&mut pos, &mut map, &mut last_path_to_home, input, output) {
+            for (pos, ch, color) in draw_instructions {
+                stdout()
+                    .execute(cursor::MoveTo(
+                        (pos.0 + DRAW_OFFSET.0) as u16,
+                        (pos.1 + DRAW_OFFSET.1) as u16,
+                    ))?
+                    .execute(PrintStyledContent(style(ch).with(color)))?;
+            }
+        } else {
+            println!("Done: {}", last_path_to_home.len());
+            break;
         }
     }
 
