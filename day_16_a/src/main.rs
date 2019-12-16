70f4179fb14c305d5b444b4646432f0b00b94f6f
Jonathan Nilsson <jonathan@voysys.se>
2019-12-17 00:56:32 +0100

    WIP

--- day_16_a/src/main.rs
+++ day_16_a/src/main.rs
@@ -1,48 +1,96 @@
 use std::error::Error;
 use std::fs;
 
-fn fft(input: &str, phases: i32) -> Result<Vec<i32>, Box<dyn Error>> {
+fn parse_input(input: &str) -> Result<Vec<i32>, Box<dyn Error>> {
+    input.trim().chars().map(|c| c.to_digit(10).map(|d| d as i32).ok_or("Not Digit".into())).collect::<Result<Vec<_>, _>>()
+}
 
-    let mut signal = input.trim().chars().map(|c| c.to_digit(10).map(|d| d as i32).ok_or("Not Digit")).collect::<Result<Vec<_>, _>>()?;
-   
-    let mut phase = signal.clone();
+fn fft_inner(signal: &[i32]) -> Vec<i32> {
+
+    let mut out = Vec::new();
+    out.resize_with(signal.len(), Default::default);
 
     let pattern = [0, 1, 0, -1];
 
-    for _ in 0..phases {
-        
-        for (i, out_digit) in phase.iter_mut().enumerate() {
+    for (i, out_digit) in out.iter_mut().enumerate() {
 
-            let mut sum = 0;
-            let mut pattern_counter = i + 1;
-            let mut pattern_digit = 1;
+        let mut sum = 0;
+        let mut pattern_counter = i + 1;
+        let mut pattern_digit = 1;
 
-            for in_digit in &signal[i..] {
+        for in_digit in &signal[i..] {
 
-                if pattern_counter == 0 {
-                    pattern_counter = i + 1;
-                    pattern_digit = (pattern_digit + 1) % pattern.len();
-                }
-
-                sum += *in_digit * pattern[pattern_digit];
-
-                pattern_counter -= 1;
+            if pattern_counter == 0 {
+                pattern_counter = i + 1;
+                pattern_digit = (pattern_digit + 1) % pattern.len();
             }
 
-            *out_digit = sum.abs() % 10;
+            sum += *in_digit * pattern[pattern_digit];
+
+            pattern_counter -= 1;
         }
 
-        signal = phase.clone();
+        *out_digit = sum.abs() % 10;
     }
 
-    Ok(signal)
+    out
+}
+
+fn fft(signal: &[i32]) -> Vec<i32> {
+
+    if signal.len() > 4 {
+
+        let mut out = Vec::new();
+        out.resize_with(signal.len(), Default::default);
+
+        let signal_len = signal.len();
+        let signal_half_len = signal_len / 2;
+
+        //let a = fft(&signal.iter().skip(1).step_by(2).copied().collect::<Vec<_>>());
+        //let b = fft(&signal.iter().step_by(2).copied().collect::<Vec<_>>());
+
+        let a = fft(&signal[..signal_half_len]);
+        let b = fft(&signal[signal_half_len..]);
+
+        for (i, out_digit) in out[..signal_half_len].iter_mut().enumerate() {
+            *out_digit = (a[i] + b[i]).abs() % 10;
+        }
+
+        for i in (signal_half_len..signal_len).rev() {
+
+            if i + 1 < signal_len {
+                out[i] = (out[i + 1] + b[i % signal_half_len]).abs() % 10;
+            } else {
+                out[i] = (b[i % signal_half_len]).abs() % 10;
+            }
+        }
+
+        out
+
+    } else {
+        fft_inner(&signal)
+    }
+}
+
+fn fft_phases(mut signal: Vec<i32>, phases: i32) -> Vec<i32> {
+
+    for i in 0..phases {
+        dbg!(i);
+        signal = fft(&signal);
+    }
+
+    signal
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
 
+    assert_eq!(fft_phases(parse_input("12345678").unwrap(), 4), [0, 1, 0, 2, 9, 4, 9, 8]);
+
     let input = fs::read_to_string("input")?;
 
-    let output = fft(&input, 100)?;
+    let signal = parse_input(&input)?;
+
+    let output = fft_phases(signal, 100);
 
     println!("{:?}", &output[..8]);
 
@@ -52,13 +100,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 #[cfg(test)]
 mod tests {
 
-    use super::fft;
+    use super::{fft_phases, parse_input};
 
     #[test]
     fn test_fft() {
-        assert_eq!(fft("12345678", 4).unwrap(), [0, 1, 0, 2, 9, 4, 9, 8]);
-        assert_eq!(fft("80871224585914546619083218645595", 100).unwrap()[..8], [2, 4, 1, 7, 6, 1, 7, 6]);
-        assert_eq!(fft("19617804207202209144916044189917", 100).unwrap()[..8], [7, 3, 7, 4, 5, 4, 1, 8]);
-        assert_eq!(fft("69317163492948606335995924319873", 100).unwrap()[..8], [5, 2, 4, 3, 2, 1, 3, 3]);
+        assert_eq!(fft_phases(parse_input("12345678").unwrap(), 4), [0, 1, 0, 2, 9, 4, 9, 8]);
+        assert_eq!(fft_phases(parse_input("80871224585914546619083218645595").unwrap(), 100)[..8], [2, 4, 1, 7, 6, 1, 7, 6]);
+        assert_eq!(fft_phases(parse_input("19617804207202209144916044189917").unwrap(), 100)[..8], [7, 3, 7, 4, 5, 4, 1, 8]);
+        assert_eq!(fft_phases(parse_input("69317163492948606335995924319873").unwrap(), 100)[..8], [5, 2, 4, 3, 2, 1, 3, 3]);
     }
 }
--- day_16_b/src/main.rs
+++ day_16_b/src/main.rs
@@ -1,13 +1,81 @@
 use std::error::Error;
 use std::fs;
 
+fn parse_input(input: &str) -> Result<Vec<i32>, Box<dyn Error>> {
+    input.trim().chars().map(|c| c.to_digit(10).map(|d| d as i32).ok_or("Not Digit".into())).collect::<Result<Vec<_>, _>>()
+}
+
+fn fft_inner(signal: &[i32]) -> Vec<i32> {
+
+    let mut out = Vec::new();
+    out.resize_with(signal.len(), Default::default);
+
+    let pattern = [0, 1, 0, -1];
+
+    for (i, out_digit) in out.iter_mut().enumerate() {
+
+        let mut sum = 0;
+        let mut pattern_counter = i + 1;
+        let mut pattern_digit = 1;
+
+        for in_digit in &signal[i..] {
+
+            if pattern_counter == 0 {
+                pattern_counter = i + 1;
+                pattern_digit = (pattern_digit + 1) % pattern.len();
+            }
+
+            sum += *in_digit * pattern[pattern_digit];
+
+            pattern_counter -= 1;
+        }
+
+        *out_digit = sum.abs() % 10;
+    }
+
+    out
+}
+
+fn fft(signal: &[i32]) -> Vec<i32> {
+
+    if signal.len() > 32 {
+
+        let mut out = Vec::new();
+        out.resize_with(signal.len(), Default::default);
+
+        let signal_half_len = signal.len() / 2;
+
+        let a = fft(&signal[..signal_half_len]);
+        let b = fft(&signal[signal_half_len..]);
+
+        for (i, out_digit) in out[..signal_half_len].iter_mut().enumerate() {
+            *out_digit = (a[i] + b[i]).abs() % 10;
+        }
+
+        out
+
+    } else {
+        fft_inner(&signal)
+    }
+}
+
+fn fft_phases(mut signal: Vec<i32>, phases: i32) -> Vec<i32> {
+
+    for i in 0..phases {
+        dbg!(i);
+        signal = fft(&signal);
+    }
+
+    signal
+}
+
 fn fft_b(input: &str, phases: i32) -> Result<Vec<i32>, Box<dyn Error>> {
 
     let offset = input[..8].parse::<usize>()?;
 
-    let mut signal = {
+    let signal = {
         
-        let initital_signal = input.trim().chars().map(|c| c.to_digit(10).map(|d| d as i32).ok_or("Not Digit")).collect::<Result<Vec<_>, _>>()?;
+        let initital_signal = parse_input(&input)?;
         
         let mut signal = Vec::new();
         signal.resize_with(initital_signal.len() * 10_000, Default::default);
@@ -19,37 +87,9 @@ fn fft_b(input: &str, phases: i32) -> Result<Vec<i32>, Box<dyn Error>> {
         signal
     };
 
-    let mut phase = signal.clone();
+    let output = fft_phases(signal, phases);
 
-    let pattern = [0, 1, 0, -1];
-
-    for p in 0..phases {
-
-        for (i, out_digit) in phase.iter_mut().enumerate() {
-
-            let mut sum = 0;
-            let mut pattern_counter = i + 1;
-            let mut pattern_digit = 1;
-
-            for in_digit in &signal[i..] {
-
-                if pattern_counter == 0 {
-                    pattern_counter = i + 1;
-                    pattern_digit = (pattern_digit + 1) % pattern.len();
-                }
-
-                sum += *in_digit * pattern[pattern_digit];
-
-                pattern_counter -= 1;
-            }
-
-            *out_digit = sum.abs() % 10;
-        }
-
-        signal = phase.clone();
-    }
-
-    Ok(signal[offset..(offset + 8)].to_vec())
+    Ok(output[offset..(offset + 8)].to_vec())
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
