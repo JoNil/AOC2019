817153c65fd38f940ed5ac5e16167927d39b68cd
Jonathan Nilsson <jonathan@voysys.se>
2019-12-24 11:28:51 +0100

    Added skeleton for rest

---
+++ day_25_b/src/main.rs
@@ -0,0 +1,18 @@
+use int_comp::IntcodeComputer;
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("input")?;
+
+    let program = input
+        .split(",")
+        .map(|v| v.trim().parse::<i64>())
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let mut incode = IntcodeComputer::new(&program);
+    
+    let output = incode.run(&[], None)?;
+
+    Ok(())
+}
