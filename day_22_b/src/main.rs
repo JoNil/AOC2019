1f7748986628eb670e8a546fdf021268d132b7d1
Jonathan Nilsson <jonathan@voysys.se>
2019-12-28 02:30:38 +0100

    Day 22 b

--- day_22_b/src/main.rs
+++ day_22_b/src/main.rs
@@ -1,55 +1,53 @@
-use num_integer::Integer;
 use std::error::Error;
 use std::fs;
 use std::time::Instant;
 
-#[derive(Copy, Clone, Debug)]
-enum Operation {
-    DealIntoNewStack,
-    DealWithIncrement(i64),
-    Cut(i32),
-}
+// https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+fn combine(f: impl Fn(i64, i64, i64) -> i64, unit: i64, mut a: i64, mut b: i64, m: i64) -> i64 {
+    let mut r = unit;
 
-impl Operation {
-    fn apply(&self, tracked_pos: i64, len: i64) -> i64 {
-        match self {
-            &Operation::DealIntoNewStack => len - tracked_pos - 1,
-            &Operation::DealWithIncrement(e_gcd) => {
-                //(num*x) % len = tracked_pos
-
-                let mut res = ((tracked_pos as i128 * e_gcd as i128) % len as i128) as i64;
-
-                while res < 0 {
-                    res += len;
-                }
-
-                res
-            }
-            &Operation::Cut(val) => {
-                let num = -val as i64;
-
-                if num > 0 {
-                    if tracked_pos < num {
-                        tracked_pos + len - num
-                    } else {
-                        tracked_pos - num
-                    }
-                } else {
-                    let inv_num = num.abs();
-
-                    if len - inv_num > tracked_pos {
-                        tracked_pos + inv_num
-                    } else {
-                        tracked_pos - len + inv_num
-                    }
-                }
-            }
+    loop {
+        if b == 0 {
+            return r;
         }
+
+        if b & 1 > 0 {
+            r = f(r, a, m);
+        }
+
+        b >>= 1;
+        a = f(a, a, m);
     }
 }
 
-fn parse_operations(input: &str, len: i64) -> Vec<Operation> {
-    let mut res = Vec::new();
+// +  (mod m)
+fn add(a: i64, b: i64, m: i64) -> i64 {
+    return (m + (a + b) % m) % m;
+}
+
+// *  (mod m)
+fn mul(a: i64, b: i64, m: i64) -> i64 {
+    return combine(add, 0, a, b, m);
+}
+
+// ** (mod m)
+fn pow(a: i64, b: i64, m: i64) -> i64 {
+    return combine(mul, 1, a, b, m);
+}
+
+#[derive(Copy, Clone, Debug)]
+struct Operation {
+    a: i64,
+    b: i64,
+    len: i64,
+}
+
+fn parse_operation(input: &str, len: i64) -> Operation {
+    let mut operation = Operation {
+        a: 1,
+        b: 0,
+        len: len,
+    };
 
     for line in input.lines() {
         let line = line.trim();
@@ -57,155 +55,51 @@ fn parse_operations(input: &str, len: i64) -> Vec<Operation> {
         if line.starts_with("deal with increment ") {
             if let Some(num) = line.split("deal with increment ").nth(1) {
                 if let Ok(num) = num.parse::<i64>() {
-                    let e = i64::extended_gcd(&num, &len);
-                    res.push(Operation::DealWithIncrement(e.x));
+                    operation.a = mul(operation.a, num, len);
+                    operation.b = mul(operation.b, num, len);
                 }
             }
         } else if line.starts_with("cut ") {
             if let Some(num) = line.split("cut ").nth(1) {
-                if let Ok(num) = num.parse::<i32>() {
-                    res.push(Operation::Cut(num));
+                if let Ok(num) = num.parse::<i64>() {
+                    operation.b = add(operation.b, -num, len);
                 }
             }
         } else if line.starts_with("deal into new stack") {
-            res.push(Operation::DealIntoNewStack);
+            operation.a = add(0, -operation.a, len);
+            operation.b = add(-1, -operation.b, len);
         }
     }
 
-    res
+    operation
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("input")?;
 
-    let mut tracked_pos = 2020;
+    let tracked_pos = 2020;
     let stack_len = 119_315_717_514_047_i64;
+    let n = 101_741_582_076_661_i64;
 
-    let mut operations = parse_operations(&input, stack_len);
-    operations.reverse();
+    let operation = parse_operation(&input, stack_len);
 
-    let mut start = Instant::now();
+    let x = mul(
+        operation.b,
+        pow(operation.a - 1, stack_len - 2, stack_len),
+        stack_len,
+    );
 
-    // loops at 5003 in a
-    // Loop will be at some prime no, len is always prime
+    let res = add(
+        mul(
+            add(x, tracked_pos, stack_len),
+            pow(pow(operation.a, stack_len - 2, stack_len), n, stack_len),
+            stack_len,
+        ),
+        -x,
+        stack_len,
+    );
 
-    for i in 0..101_741_582_076_661_i64 {
-        if i % 1_000_000_i64 == 0 {
-            let now = Instant::now();
-            println!(
-                "{}: {}",
-                i / 1_000_000,
-                (now - start).as_secs_f64() * 1000.0
-            );
-            start = now;
-        }
-
-        for operation in &operations {
-            tracked_pos = operation.apply(tracked_pos, stack_len);
-        }
-
-        if tracked_pos == 2020 {
-            println!("Found loop at {}", i);
-            break;
-        }
-    }
-
-    println!("{:#?}", tracked_pos);
+    println!("{:#?}", res);
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::parse_operations;
-
-    #[test]
-    fn test_22_b() {
-        {
-            let input = "deal with increment 7
-                deal into new stack
-                deal into new stack";
-
-            let mut operations = parse_operations(&input, 10);
-            operations.reverse();
-
-            let inputs = [0, 3, 6, 9, 2, 5, 8, 1, 4, 7];
-
-            for (index, _) in inputs.iter().enumerate() {
-                let mut res = index as i64;
-                for operation in &operations {
-                    res = operation.apply(res, inputs.len() as i64);
-                }
-
-                assert_eq!(res, inputs[index as usize]);
-            }
-        }
-
-        {
-            let input = "cut 6
-                deal with increment 7
-                deal into new stack";
-
-            let mut operations = parse_operations(&input, 10);
-            operations.reverse();
-
-            let inputs = [3, 0, 7, 4, 1, 8, 5, 2, 9, 6];
-
-            for (index, _) in inputs.iter().enumerate() {
-                let mut res = index as i64;
-                for operation in &operations {
-                    res = operation.apply(res, inputs.len() as i64);
-                }
-
-                assert_eq!(res, inputs[index as usize]);
-            }
-        }
-
-        {
-            let input = "deal with increment 7
-                deal with increment 9
-                cut -2";
-
-            let mut operations = parse_operations(&input, 10);
-            operations.reverse();
-
-            let inputs = [6, 3, 0, 7, 4, 1, 8, 5, 2, 9];
-
-            for (index, _) in inputs.iter().enumerate() {
-                let mut res = index as i64;
-                for operation in &operations {
-                    res = operation.apply(res, inputs.len() as i64);
-                }
-
-                assert_eq!(res, inputs[index as usize]);
-            }
-        }
-
-        {
-            let input = "deal into new stack
-                cut -2
-                deal with increment 7
-                cut 8
-                cut -4
-                deal with increment 7
-                cut 3
-                deal with increment 9
-                deal with increment 3
-                cut -1";
-
-            let mut operations = parse_operations(&input, 10);
-            operations.reverse();
-
-            let inputs = [9, 2, 5, 8, 1, 4, 7, 0, 3, 6];
-
-            for (index, _) in inputs.iter().enumerate() {
-                let mut res = index as i64;
-                for operation in &operations {
-                    res = operation.apply(res, inputs.len() as i64);
-                }
-
-                assert_eq!(res, inputs[index as usize]);
-            }
-        }
-    }
 }
